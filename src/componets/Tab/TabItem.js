1a440b537656f7b0e0446979724eca50f164665f
Chris <raoxiaochuan@pvmedtech.com>
2021-09-14 18:12:23 +0800

    封装Tab组件

---
+++ src/componets/Tab/TabItem.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Chris
+ * @Date: 2021-09-14 17:23:56
+ * @LastEditors: Chris
+ * @LastEditTime: 2021-09-14 17:56:47
+ * @Descripttion: **
+ */
+const Index = ({children}) => {
+  return <div>{children}</div>
+}
+Index.displayName = 'TabItem'
+export default Index;
