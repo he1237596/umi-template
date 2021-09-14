0f9c4235c3204405898454994763c40495045b98
Chris <raoxiaochuan@pvmedtech.com>
2021-09-14 18:12:51 +0800

    引用组件

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,23 @@
 import styles from './index.css';
 import { formatMessage } from 'umi-plugin-locale';
+import Tab from '../componets/Tab'
+import TabItem from '../componets/Tab/TabItem'
 export default function() {
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
+      {/* <div className={styles.welcome} /> */}
+      {/* <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
           <a href="https://umijs.org/guide/getting-started.html">
             {formatMessage({ id: 'index.start' })}
           </a>
         </li>
-      </ul>
+      </ul> */}
+      <Tab onChange={()=>{}}>
+        <TabItem name='vue' label='vue'>vue</TabItem>
+        <TabItem name='react' label='react'>react</TabItem>
+      </Tab>
     </div>
   );
 }
