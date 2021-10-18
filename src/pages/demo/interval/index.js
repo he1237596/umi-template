2912d6d7f1e6ccfb19cffe419d2c174540a9dbaa
Chris <raoxiaochuan@pvmedtech.com>
2021-10-18 16:50:09 +0800

    定时器的一种写法

--- src/pages/demo/interval/index.js
+++ src/pages/demo/interval/index.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, forwardRef, useImperativeHandle, useRef } from 'react'
+import { useState, useEffect } from 'react'
 // 通常数据流是从上到下，可以在子组件里改变父组件的数据
 export default function Counter() {
   const [count, setCount] = useState(0);
@@ -8,7 +8,7 @@ export default function Counter() {
       setCount(c => c + 1); // ✅ 在这不依赖于外部的 `count` 变量
     }, 1000);
     return () => clearInterval(id);
-  }, []); // ✅ 我们的 effect 不适用组件作用域中的任何变量
+  }, []); // ✅ 我们的 effect 不使用组件作用域中的任何变量
 
   return <h1>{count}</h1>;
 }
